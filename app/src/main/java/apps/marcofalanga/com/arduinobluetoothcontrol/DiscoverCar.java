9e0f4b47e4b7760426b66ec50cdf112dce4c068e
MuSkinny <marco.fala97@gmail.com>
2016-02-28 02:00:29 +0100

    Initial commit

---
+++ app/src/main/java/apps/marcofalanga/com/arduinobluetoothcontrol/DiscoverCar.java
@@ -0,0 +1,13 @@
+package apps.marcofalanga.com.arduinobluetoothcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiscoverCar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover_car);
+    }
+}
